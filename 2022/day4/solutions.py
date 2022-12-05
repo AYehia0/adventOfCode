051d108c57c3cdfc20a29531d2ad2713cb13f660
AYehia0 <ahmedyehia611@gmail.com>
2022-12-05 03:40:27 +0200

    Day 4 [MOD] : smart math way.

--- 2022/day4/solutions.py
+++ 2022/day4/solutions.py
@@ -10,11 +10,17 @@ with open("input.txt") as df:
         a1, a2 = list(map(int, p1.split("-")))
         b1, b2 = list(map(int, p2.split("-")))
 
-        s1, s2 = set(range(a1, a2 + 1, 1)), set(range(b1, b2 + 1, 1))
-        if s1.issubset(s2) or s2.issubset(s1):
+        # math way
+        if a1 <= b1 and b2 <= a2 or b1 <= a1 and a2 <= b2:
             ans1 += 1
-        if s1.intersection(s2):
+
+        if not (a2 < b1 or a1 > b2):
             ans2 += 1
+        # s1, s2 = set(range(a1, a2 + 1, 1)), set(range(b1, b2 + 1, 1))
+        # if s1.issubset(s2) or s2.issubset(s1):
+        #     ans1 += 1
+        # if s1.intersection(s2):
+        #     ans2 += 1
 
     print(f"Part 1 : {ans1}")
     print(f"Part 2 : {ans2}")
