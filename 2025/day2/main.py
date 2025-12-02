da61b7692059405bdf2dcbdf59747b0a20e9d0e9
AYehia0 <ahmedyehia611@gmail.com>
2025-12-03 00:48:29 +0200

    day2: part2 easy with classical dups

--- 2025/day2/main.py
+++ 2025/day2/main.py
@@ -5,6 +5,9 @@ def is_valid(num):
     mid = len(num) // 2
     return num[0:mid] != num[mid:]
 
+def is_valid_part2(num: str) -> bool:
+    return not (num in (num + num)[1:-1])
+
 def part1(readings):
     ans = 0
     for reading in readings:
@@ -14,10 +17,14 @@ def part1(readings):
                 ans += i
     return ans
 
-
-    
 def part2(readings):
-    pass
+    ans = 0
+    for reading in readings:
+        id1, id2 = list(map(int, reading.split("-")))
+        for i in range(id1, id2+1, 1):
+            if not is_valid_part2(str(i)):
+                ans += i
+    return ans
 
 def main():
     print("Ans 1: ", part1(data))
