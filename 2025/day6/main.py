e11d38dcaa3eef1b8bcf0d4ebe570ea3dfe447a4
AYehia0 <ahmedyehia611@gmail.com>
2025-12-09 00:39:21 +0200

    day6: part 2 cool zip

--- 2025/day6/main.py
+++ 2025/day6/main.py
@@ -1,67 +1,43 @@
+from operator import mul
+from functools import reduce
+
 with open("input.txt") as df:
-    data = [l.rstrip("\n") for l in df.read().splitlines()]
-
-def split_into_problems(matrix):
-    width = max(len(row) for row in matrix)
-    mat = [row.ljust(width) for row in matrix]
-
-    problems = []
-    cur = []
-
-    for col in range(width):
-        column = [mat[row][col] for row in range(len(mat))]
-
-        if all(c == ' ' for c in column):
-            if cur:
-                problems.append(cur)
-                cur = []
-        else:
-            cur.append(column)
-
-    if cur:
-        problems.append(cur)
-
-    return problems
-
-
-def parse_problem(columns):
-    rows = ["".join(col[i] for col in columns) for i in range(len(columns[0]))]
-
-    op = rows[-1].strip()
-
-    nums = [int(r.strip()) for r in rows[:-1] if r.strip()]
-
-    return nums, op
-
-
-def evaluate(nums, op):
-    res = nums[0]
-    for x in nums[1:]:
-        if op == '*':
-            res *= x
-        else:
-            res += x
-    return res
-
+    lines = df.read().splitlines()
+    data2 = [line.strip("\n") for line in lines]
+    data = [line.strip().split() for line in lines]
 
 def part1(data):
-    problems = split_into_problems(data)
-    total = 0
+    ans = 0
+    cols = list(zip(*data))
+    for col in cols:
+        op = col[-1]
+        nums = map(int, col[:-1])
+        if op == '*':
+            ans += reduce(mul, [n for n in nums])
+        if op == '+':
+            ans += sum([n for n in nums])
 
-    for p in problems:
-        nums, op = parse_problem(p)
-        total += evaluate(nums, op)
-
-    return total
-
+    return ans
 
 def part2(data):
-    pass
-
+    ans = 0
+    cols = list(zip(*data))
+    groups = []
+    curr = []
+    for col in cols[::-1]:
+        if set(col) == {" "}:
+            groups.append(curr)
+            curr = []
+        else:
+            curr.append(col)
+    groups.append(curr)
+    for group in groups:
+        ans += eval(group[-1][-1].join("".join(a[:-1]) for a in group))
+    return ans
 
 def main():
     print("Ans 1:", part1(data))
-    print("Ans 2:", part2(data))
+    print("Ans 2:", part2(data2))
 
 if __name__ == "__main__":
     main()
